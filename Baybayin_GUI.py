1cc0677e98b57664e1a6cb7833f700469b98a2f5
wassupea <62280250+wassupea@users.noreply.github.com>
2022-02-24 17:32:10 +0800

    Update Baybayin_GUI.py

--- Baybayin_GUI.py
+++ Baybayin_GUI.py
@@ -1,7 +1,9 @@
 from tkinter import *
 import tkinter as tk
 from PIL import ImageGrab , ImageTk ,Image
-import pyscreenshot
+import cv2
+import matplotlib.pyplot as plt
+import numpy as np
 
 def draw(event):
     x , y = event.x,event.y
@@ -20,17 +22,41 @@ def clear_canvas():
     canvas.delete("all")
 
 def classify_char():
-    #
-    #im = ImageGrab.grab(bbox=(321,130,1300,510))
-    #im.save('./written_chars/sulat.png','PNG')
-    #im = ImageGrab.grab(bbox=(321,130,1000,510))
-    #im.save('./written_chars/sulat2.png','PNG')
-    #im = ImageGrab.grab(bbox=(350,130,1000,510))
-    #im.save('./written_chars/sulat3.png','PNG')
-    #im = ImageGrab.grab(bbox=(350,200,1000,10))
-    #im.save('./written_chars/sulat4.png','PNG')
-    im = ImageGrab.grab(bbox=(351,200,1000,550))
-    im.save('./written_chars/sulat5.png','PNG')
+    written_char = ImageGrab.grab(bbox=(400,215,780,530))
+    written_char.save('./written_chars/sulat.png','PNG')
+    digit=recognize_char(written_char)
+    written_char.show()
+
+def recognize_char(image):
+    char_image = cv2.imread('./written_chars/sulat.png')
+    grey = cv2.cvtColor(char_image.copy(), cv2.COLOR_BGR2GRAY)
+    returns, thresh = cv2.threshold(grey.copy(), 75, 255, cv2.THRESH_BINARY_INV)
+    contours, hierachy = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    preprocessed_digits = []
+
+    for c in contours:
+        x, y, w, h = cv2.boundingRect(c)
+
+        # Creating a rectangle around the digit in the original image (for displaying the digits fetched via contours)
+        cv2.rectangle(char_image, (x, y), (x + w, y + h), color=(0, 255, 0), thickness=2)
+
+        # Cropping out the digit from the image corresponding to the current contours in the for loop
+        digit = thresh[y:y + h, x:x + w]
+
+        # Resizing that digit to (18, 18)
+        resized_digit = cv2.resize(digit, (18, 18))
+
+        # Padding the digit with 5 pixels of black color (zeros) in each side to finally produce the image of (28, 28)
+        padded_digit = np.pad(resized_digit, ((5, 5), (5, 5)), "constant", constant_values=0)
+
+        # Adding the preprocessed digit to the list of preprocessed digits
+        preprocessed_digits.append(padded_digit)
+        print("\n\n\n----------------Contoured Image--------------------")
+        plt.imshow(char_image, cmap="gray")
+        plt.show()
+        print("END")
+        print("PADDED DIGIT : ", padded_digit)
+
 
 def center_window():
     # Putting window in center
