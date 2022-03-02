fe1655707282de16b219c4ace904289f55b608dd
wassupea <62280250+wassupea@users.noreply.github.com>
2022-03-02 20:35:59 +0800

    isang char pa lang narerecognize

--- Baybayin_GUI.py
+++ Baybayin_GUI.py
@@ -1,14 +1,15 @@
 from tkinter import *
 import tkinter as tk
 from PIL import ImageGrab , ImageTk ,Image
+from cv2 import resize
+import pyscreenshot
 import cv2
 import matplotlib.pyplot as plt
 import numpy as np
+import glob
 from keras.models import load_model
 
-
-# Load Model
-model = load_model('./baybayin_model1.h5')
+model = load_model('./baybayin_model1.h5') 
 
 def draw(event):
     x , y = event.x,event.y
@@ -16,7 +17,7 @@ def draw(event):
     r = 8
     canvas.create_oval(x-r,y-r,x+r,y+r,fill="black")
     classify_btn.configure(state=NORMAL)
-
+    
 
 # Function for exit.
 def close_canvas():
@@ -27,76 +28,7 @@ def clear_canvas():
     canvas.delete("all")
 
 
-
-#Function for capturing the written character
-def classify_char():
-    written_char = ImageGrab.grab(bbox=(400,215,780,530))
-    written_char.save('./written_chars/sulat.png','PNG')
-    digit=recognize_char(written_char)
-    written_char.show()
-    print(str(digit))
-
-    lab1 = tk.Label(window, text='Predicted Digit is : ' + output, width=24, height=2, fg="#3e7d75", bg="black",
-                   font=('Lucida Typewriter', 16, ' bold '))
-    lab1.place(x=10, y=420)
-
-    for digits in digit:
-        if digits==0:
-            output+='a'
-        elif digits==1:
-            output+='ba'
-        elif digits==2:
-            output+='da/ra'
-        elif digits==3:
-            output+='e/i'
-        elif digits==4:
-            output+='ga'
-        elif digits==5:
-            output+='ha'
-        elif digits==6:
-            output+='ka'
-        elif digits==7:
-            output+='kuw'
-        elif digits==8:
-            output+='la'
-        elif digits==9:
-            output+='ma'
-        elif digits==10:
-            output+='na'
-        elif digits==11:
-            output+='nga'
-        elif digits==12:
-            output+='o/u'
-        elif digits==13:
-            output+='pa'
-        elif digits==14:
-            output+='sa'
-        elif digits==15:
-            output+='ta' 
-        elif digits==16:
-            output+='tul'
-        elif digits==17:
-            output+='wa' # or i
-        elif digits==18:
-            output+='ya' # or u
-    return output
-
-    
         
-#Function for preprocessing the image then passing it to the model for the prediction.
-def recognize_char(image):
-    char_image = cv2.imread('./written_chars/sulat.png')
-    grey = cv2.cvtColor(char_image.copy(), cv2.COLOR_BGR2GRAY)
-    returns, thresh = cv2.threshold(grey.copy(), 75, 255, cv2.THRESH_BINARY_INV)
-    contours, hierachy = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-    preprocessed_digits = []
-    filtered_image = cv2.resize(char_image,(64,64))
-    if len(filtered_image.shape) == 3:
-        filtered_image = cv2.cvtColor(filtered_image, cv2.COLOR_BGR2GRAY)
-    
-    res = model.predict(filtered_image.reshape(1, 64, 64, 1))
-    return np.argmax(res)
-
 
 def center_window():
     # Putting window in center
@@ -105,14 +37,110 @@ def center_window():
 
     ws = window.winfo_screenwidth() #1366
     hs = window.winfo_screenheight() #768
-    #print(ws,hs)
     x = (ws / 2) - (w / 2) - 20
     y = (hs / 2) - (h / 2) - 50
-    #print(x,y)
     window.geometry('%dx%d+%d+%d' % (w, h, x, y))
     window.resizable(width=False, height=False)
 
 
+def classify_char():
+    written_char = ImageGrab.grab(bbox=(400,215,780,530))
+    written_char.save('./written_chars/sulat.png','PNG')
+    baybayin_chars = []
+    digit=preprocess_segment(written_char)
+    baybayin_chars.append(digit)
+    #written_char.show()
+    #print(str(digit))
+
+    
+
+    for baybayin_char in baybayin_chars:
+        if baybayin_char==0:
+            output ='a'
+        elif baybayin_char==1:
+            output ='ba'
+        elif baybayin_char==2:
+            output = 'da/ra'
+        elif baybayin_char==3:
+            output = 'e/i'
+        elif baybayin_char==4:
+            output == 'ga'
+        elif baybayin_char==5:
+            output ='ha'
+        elif baybayin_char==6:
+            output ='ka'
+        elif baybayin_char==7:
+            output ='kuw'
+        elif baybayin_char==8:
+            output ='la'
+        elif baybayin_char==9:
+            output ='ma'
+        elif baybayin_char==10:
+            output ='na'
+        elif baybayin_char==11:
+            output ='nga'
+        elif baybayin_char==12:
+            output ='o/u'
+        elif baybayin_char==13:
+            output ='pa'
+        elif baybayin_char==14:
+            output='sa'
+        elif baybayin_char==15:
+            output='ta' 
+        elif baybayin_char==16:
+            output='tul'
+        elif baybayin_char==17:
+            output='wa' # or i
+        elif baybayin_char==18:
+            output='ya' # or u
+
+    print(output)
+    
+    
+#function for image preprocess and image segmentation
+
+def preprocess_segment(img):
+    image = cv2.imread('./written_chars/sulat.png')
+    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    segmented = []
+    predictions = []
+    count=1
+
+    #create a binary threshold image
+
+    ret, binary = cv2.threshold(gray, 150, 255, cv2.THRESH_BINARY_INV)
+
+
+    # find the contours from the thresholded image
+    contours, hierarchy = cv2.findContours(binary, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+
+    # draw all contours
+    with_contours = cv2.drawContours(image, contours, -1, (0, 255, 0), 1)
+
+   
+    for c in contours:
+        x, y, w, h = cv2.boundingRect(c)
+        cv2.rectangle(with_contours,(x,y), (x+w,y+h), (0,255,0), 1)
+        cropped_contour= with_contours[y:y+h, x:x+w]
+        
+        resize_contour = cv2.resize(cropped_contour, (64, 64), interpolation=cv2.INTER_AREA)
+        resize_contour = cv2.cvtColor(resize_contour, cv2.COLOR_BGR2GRAY)
+        img_reshape = resize_contour.reshape(1,64,64,1)
+        image_reshape = img_reshape / 255.0
+        img_name = "written_chars" + str(count+1) +".png"
+        cv2.imwrite(img_name,resize_contour)
+        cv2.imread(img_name)
+        cv2.imshow('Image',resize_contour)
+        cv2.waitKey(0)
+        segmented.append(image_reshape)
+        print(len(segmented))
+        print('Segmented shape:', np.array(segmented).shape)
+
+        predict = model.predict([segmented])[0]
+        final_predict = np.argmax(predict)
+        print(final_predict)
+
+        return final_predict
 
 
 if __name__ == '__main__':
